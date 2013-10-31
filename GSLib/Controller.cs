71b19a65307fc9f98fe02184f10250009ae03f0a
tk1977 <tkorytnyuk@gmail.com>
2013-10-31 16:32:25 -0400

    Initial Commit

---
+++ GSLib/Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GSLib {
+    /// <summary>
+    /// Entry Point
+    /// </summary>
+    public class Controller {
+    }
+}
