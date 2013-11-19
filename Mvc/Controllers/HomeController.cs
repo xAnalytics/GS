584869b4aec2a163cd009e875cfbbacec6f3fee2
tk1977 <tkorytnyuk@gmail.com>
2013-11-19 16:57:25 -0500

    Changes to MVC project

--- Mvc/Controllers/HomeController.cs
+++ Mvc/Controllers/HomeController.cs
@@ -7,21 +7,38 @@ using System.Web.Mvc;
 namespace Mvc.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "A set of SEO tools for everybody";
 
             return View();
         }
 
         public ActionResult About() {
-            ViewBag.Message = "Your app description page.";
+            //ViewBag.Message = "Your app description page.";
 
             return View();
         }
 
         public ActionResult Contact() {
-            ViewBag.Message = "Your contact page.";
+            //ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        public ActionResult KwPosition() {
+
+
+            return View();
+        }
+
+        public ActionResult PlagiarismChecker() {
+
+            return View();
+        }
+
+        public ActionResult GooglePageRankChecker() {
+
+            return View();
+        }
+
     }
 }
